3d23ebc6a2e3ea4442ad0513ae12f0d908ed3523
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-17 09:37:56 +0700

    4. Todo List : 5. Saving Todos

--- todolist/app/Http/Controllers/TodosController.php
+++ todolist/app/Http/Controllers/TodosController.php
@@ -39,7 +39,20 @@ class TodosController extends Controller
      */
     public function store(Request $request)
     {
-        return $request->all();
+        $this->validate($request, [
+            'text' => 'required'
+        ]);
+
+        // Create Todo
+        $todo = new Todo();
+
+        $todo->text = $request->input('text');
+        $todo->body = $request->input('body');
+        $todo->due = $request->input('due');
+
+        $todo->save();
+
+        return redirect('/')->with('success', 'Todo Created');
     }
 
     /**
