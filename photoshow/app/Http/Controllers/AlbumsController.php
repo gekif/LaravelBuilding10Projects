d198610128051be9a879571a04beb71e5a2e3ea9
Gekif <dzulfikar.maulana@gmail.com>
2020-03-07 19:30:36 +0700

    6. Photo Gallery: 4. Cover Image Upload

--- photoshow/app/Http/Controllers/AlbumsController.php
+++ photoshow/app/Http/Controllers/AlbumsController.php
@@ -20,6 +20,33 @@ class AlbumsController extends Controller
 
     public function store(Request $request)
     {
-        return;
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+            'cover_image' => 'image|max:1999'
+        ];
+
+        $this->validate($request, $rules);
+
+        $uploadedFile = $request->file('cover_image');
+
+        // Get Filename With Extension
+        $fileNameWithExt = $uploadedFile->getClientOriginalName();
+
+        // Get Just File Name
+        $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+
+        // Get Extension
+        $extension = $uploadedFile->getClientOriginalExtension();
+
+        // Create New Filename
+        $fileNameToStore = $fileName . '_' . time() . '.' . $extension;
+
+        // Upload Image
+        $path = $uploadedFile
+            ->storeAs('public/album_covers', $fileNameToStore);
+
+        return $path;
     }
+
 }
--- photoshow/resources/views/layouts/app.blade.php
+++ photoshow/resources/views/layouts/app.blade.php
@@ -9,11 +9,16 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.min.css">
 </head>
 
+<body>
+
+@include('inc.topbar')
+<br>
+
 <div class="row">
+    @include('inc.messages')
     @yield('content')
 </div>
 
-<body>
-
 </body>
+
 </html>
